624aa1a6a9cdea92182148c4b56f04819441b36d
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2022-08-25 18:35:45 +0530

    validation trigger lambda creation added

---
+++ infra/cdk/stack_blueprints/lambda_construct.py
@@ -0,0 +1,80 @@
+"""Code for generation and deploy lambda to AWS"""
+import json
+import aws_cdk.aws_iam as iam
+import aws_cdk.aws_lambda as aws_lambda
+import aws_cdk.aws_logs as aws_logs
+from aws_cdk import Stack, Duration
+
+
+class LambdaConstruct:
+    """Class with static methods that are used to build and deploy lambdas."""
+
+    @staticmethod
+    def create_lambda(
+            stack: Stack,
+            config: dict,
+            # env: str,
+            lambda_name: str,
+            role: iam.Role,
+            duration: Duration = None) -> aws_lambda.Function:
+        """Method called by construct for creating lambda."""
+
+        return LambdaConstruct.create_lambda_function(
+            stack=stack,
+            config=config,
+            lambda_name=lambda_name,
+            role=role,
+            duration=duration
+        )
+
+    @staticmethod
+    def create_lambda_function(
+            stack: Stack,
+            config: dict,
+            lambda_name: str,
+            role: iam.Role,
+            duration: Duration) -> aws_lambda.Function:
+        """Methods for generic lambda creation."""
+
+        env_vars = json.loads(config['global'][f"{lambda_name}Environment"])
+        lambda_path = config['global'][f"{lambda_name}HndlrPath"]
+        handler = config['global'][f"{lambda_name}Hndlr"]
+        function_id = f"{config['global']['app-name']}-{lambda_name}-Id"
+        function_name = f"{config['global']['app-name']}-{lambda_name}"
+
+        dict_props = {
+            "function_name": function_name,
+            "code": aws_lambda.Code.from_asset(path=lambda_path),
+            "handler": handler,
+            "runtime": aws_lambda.Runtime.PYTHON_3_8,
+            "role": role,
+            "environment": env_vars,
+            "architecture": aws_lambda.Architecture.ARM_64,
+            "timeout": duration,
+            "log_retention": aws_logs.RetentionDays.THREE_MONTHS
+        }
+
+        return aws_lambda.Function(scope=stack, id=function_id, **dict_props)        
+
+    @staticmethod
+    def get_cloudwatch_policy(lambda_logs_arn: str) -> iam.PolicyStatement:
+        """Returns policy statement for creating logs."""
+        policy_statement = iam.PolicyStatement()
+        policy_statement.effect = iam.Effect.ALLOW
+        policy_statement.add_actions("cloudwatch:PutMetricData")
+        policy_statement.add_actions("logs:CreateLogGroup")
+        policy_statement.add_actions("logs:CreateLogStream")
+        policy_statement.add_actions("logs:GetQueryResults")
+        policy_statement.add_actions("logs:PutLogEvents")
+        policy_statement.add_actions("logs:StartQuery")
+        policy_statement.add_resources(lambda_logs_arn)
+        return policy_statement
+
+    @staticmethod
+    def get_sfn_execute_policy(step_function_arn: str) -> iam.PolicyStatement:
+        """Returns policy statements for executing step function."""
+        policy_statement = iam.PolicyStatement()
+        policy_statement.effect = iam.Effect.ALLOW
+        policy_statement.add_actions("states:StartExecution")
+        policy_statement.add_resources(step_function_arn)
+        return policy_statement
--- infra/cdk/stack_blueprints/stack.py
+++ infra/cdk/stack_blueprints/stack.py
@@ -3,11 +3,13 @@ from typing import Dict, Any
 import aws_cdk
 import aws_cdk.aws_iam as iam
 import aws_cdk.aws_kms as kms
+import aws_cdk.aws_lambda as _lambda
 from constructs import Construct
 
 from .iam_construct import IAMConstruct
 from .kms_construct import KMSConstruct
 from .s3_construct import S3Construct
+from .lambda_construct import LambdaConstruct
 
 
 class MainProjectStack(aws_cdk.Stack):
@@ -18,12 +20,13 @@ class MainProjectStack(aws_cdk.Stack):
         super().__init__(scope, app_id, **kwargs)
         self.env_var = env_var
         self.config = config
-        MainProjectStack.create_stack(self, config=self.config)
+        MainProjectStack.create_stack(self, config=self.config, env=self.env_var)
 
     @staticmethod
     def create_stack(
             stack: aws_cdk.Stack,  
-            config: dict) -> None:
+            config: dict,
+            env: str) -> None:
         """Create and add the resources to the application stack"""
 
         # KMS infra setup ------------------------------------------------------
@@ -43,6 +46,15 @@ class MainProjectStack(aws_cdk.Stack):
             kms_key=kms_key
         )
         print(stack_role)
+
+        # Infra for Lambda function creation -------------------------------------
+        lambdas = MainProjectStack.create_lambda_functions(
+            stack=stack,
+            config=config,
+            # env=env,
+            kms_key=kms_key
+        )
+        print(lambdas)
 
     @staticmethod
     def create_stack_role(
@@ -70,4 +82,52 @@ class MainProjectStack(aws_cdk.Stack):
             assumed_by=["s3", "lambda"]
         )
         stack_role.add_managed_policy(policy=stack_policy)
-        return stack_role  
+        return stack_role
+
+    @staticmethod
+    def create_lambda_functions(
+            stack: aws_cdk.Stack,
+            config: dict,
+            # env: str,
+            kms_key: kms.Key) -> Dict[str, _lambda.Function]:
+        """Create placeholder lambda function and roles."""
+
+        lambdas = {}
+
+        # Validation Trigger Lambda. ----------------------------------------------------
+        validation_trigger_policy = IAMConstruct.create_managed_policy(
+            stack=stack,
+            config=config,
+            policy_name="validation_trigger",
+            statements=[
+                LambdaConstruct.get_sfn_execute_policy(
+                    config['global']['stepFunctionArn']  
+                ),
+                LambdaConstruct.get_cloudwatch_policy(
+                    config['global']['validation_trigger_lambdaLogsArn']
+                ),
+                S3Construct.get_s3_object_policy(config['global']['bucket_arn']),
+                S3Construct.get_s3_bucket_policy(config['global']['bucket_arn']),
+                KMSConstruct.get_kms_key_encrypt_decrypt_policy([kms_key.key_arn])
+            ]
+        )
+
+        validation_trigger_role = IAMConstruct.create_role(
+            stack=stack,
+            config=config,
+            role_name="validation_trigger",
+            assumed_by=["lambda", "s3"]   
+        )
+
+        validation_trigger_role.add_managed_policy(validation_trigger_policy)
+
+        lambdas["validation_trigger_lambda"] = LambdaConstruct.create_lambda(
+            stack=stack,
+            config=config,
+            # env=env,
+            lambda_name="validation_trigger_lambda",
+            role=validation_trigger_role,
+            duration=aws_cdk.Duration.minutes(15)
+        )
+
+        return lambdas
