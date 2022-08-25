e4d665e7a833a083a04eca13c0cc27c899031cbb
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2022-08-25 12:03:36 +0530

    IAM role creation added

--- infra/cdk/stack_blueprints/stack.py
+++ infra/cdk/stack_blueprints/stack.py
@@ -1,10 +1,13 @@
 """Main python file_key for adding resources to the application stack."""
 from typing import Dict, Any
 import aws_cdk
+import aws_cdk.aws_iam as iam
+import aws_cdk.aws_kms as kms
 from constructs import Construct
 
 from .iam_construct import IAMConstruct
 from .kms_construct import KMSConstruct
+from .s3_construct import S3Construct
 
 
 class MainProjectStack(aws_cdk.Stack):
@@ -32,3 +35,40 @@ class MainProjectStack(aws_cdk.Stack):
             policy_doc=kms_pol_doc
         )
         print(kms_key)
+
+        # IAM Role Setup --------------------------------------------------------
+        stack_role = MainProjectStack.create_stack_role(
+            config=config,
+            stack=stack,
+            kms_key=kms_key
+        )
+        print(stack_role)
+
+    @staticmethod
+    def create_stack_role(
+        config: dict,
+        stack: aws_cdk.Stack,
+        kms_key: kms.Key
+    ) -> iam.Role:
+        """Create the IAM role."""
+
+        stack_policy = IAMConstruct.create_managed_policy(
+            stack=stack,
+            config=config,
+            policy_name="mainStack",
+            statements=[
+                KMSConstruct.get_kms_key_encrypt_decrypt_policy(
+                    [kms_key.key_arn]
+                ),
+                S3Construct.get_s3_object_policy([config['global']['bucket_arn']]),
+            ]
+        )
+        stack_role = IAMConstruct.create_role(
+            stack=stack,
+            config=config,
+            role_name="mainStack",
+            assumed_by=["s3", "lambda"]
+        )
+        stack_role.add_managed_policy(policy=stack_policy)
+        return stack_role
+        
