710491045962737ae0859f5429078aafaec9d40d
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2022-08-25 18:49:09 +0530

    Lambda code added

---
+++ src/validation_trigger/main.py
@@ -0,0 +1,32 @@
+"""Runs as Validation Trigger Lambda.
+
+Raises-
+    error: lambda exception
+Returns-
+    [dict]:
+
+"""
+import os
+from datetime import datetime
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+
+def lambda_handler(event: dict, _context: dict) -> dict:
+    """Main lambda handler for Incoming Data to S3 Transform Location Lambda."""
+
+    # In case a correct event is encountered -------------------------------------------
+    if event:
+        try:
+            logging.info("This is the event we received: %s", event)
+            return {
+                'Success': "Event found"
+            }
+        # In case an error is encountered even when a correct event is present ---------
+        except Exception as error:
+            logging.error("An error occurred: %s", error)
+            raise error
+    # In case no event is present ------------------------------------------------------
+    else:
+        logging.error("We couldn't find a suitable event. Exiting....")
+        raise OSError("No event found")
